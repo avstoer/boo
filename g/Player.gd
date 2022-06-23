be04f1c5af4e2cea82083869f5f10de9625220a2
avstør <71973731+avstoer@users.noreply.github.com>
2022-06-23 19:24:24 +0530

    Update Player.gd

--- g/Player.gd
+++ g/Player.gd
@@ -1,20 +1,34 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 100
-const ACCELERATION = 10
+const MAX_SPEED = 40
+const SPRINT_SPEED = 160
+const ACCELERATION = 4
+const SPRINTING = 8
 const FRICTION = 10
+const MAX_STAMINA = 2
 
 var velocity = Vector2.ZERO
-#yay gaem haha
+var stamina = MAX_STAMINA
+var time = 0
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	print(stamina, " : ", time)
+	if not Input.is_action_pressed("sprint") and stamina < MAX_STAMINA:
+			stamina += delta * 0.4
+			time += delta
 	
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
+		if Input.is_action_pressed("sprint") and stamina > 0:
+			velocity = velocity.move_toward(input_vector * SPRINT_SPEED, SPRINTING)
+			stamina -= delta
+		else:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
 	move_and_slide(velocity)
+	look_at(get_global_mouse_position())
